26117ed3d772ebbc5737b3f10887cef5c53888cf
shobhi10058 <rkgupta462gkp@gmail.com>
2020-12-02 00:53:52 +0530

    Updated Remove Options

--- KeepPages.py
+++ KeepPages.py
@@ -1,4 +1,4 @@
-import os, click, validators, webbrowser
+import os, click, validators, webbrowser, shutil
 from pathlib import Path
 
 @click.group()
@@ -11,16 +11,19 @@ def main():
     os.chdir(home)
     if not os.path.exists("blogs"):
         os.makedirs('blogs')    
-    global dic, topics
+    global dic, topics, dic_top_pag
     topics = set(os.listdir('blogs'))
     dic = {}
+    dic_top_pag = {}
     os.chdir('blogs')
     for Topic in topics:
         for r, d, files in os.walk(Topic):
+            dic_top_pag[Topic] = set()
             for f in files:
                 file = open(Topic + "/" + f, 'r')
                 name, url = file.readline()[:-1], file.readline()[:-1]
-                dic[name] = url
+                dic_top_pag[Topic].add(name)
+                dic[name] = [url, Topic]
                 file.close()
     pass
 
@@ -31,7 +34,6 @@ def main():
 @click.argument('url')
 def AddBlog(topic, name, url):
     """ Add your favourite Page"""
-    #click.echo(" ".join([topic,name,url]))
     make = True
     """Keep the name and topic descriptive"""
     if(validators.url(url)):
@@ -40,7 +42,7 @@ def AddBlog(topic, name, url):
                 click.echo("Try other name, this name is already used")
                 make = False
             else:
-                dic[name] = url
+                dic[name] = [url, topic]
                 click.echo("Your webpage is saved, you can open it using the openyourpage command")
         else:
             os.makedirs(topic)
@@ -58,28 +60,44 @@ def Open(name):
     """Open the webpage in your browser"""
     if(name in dic):
         click.echo('Opening in your browser')
-        webbrowser.open_new_tab(dic[name])    
+        webbrowser.open_new_tab(dic[name][0])    
     else:
         click.echo("No such pages are saved, try first saving it")
 
 @main.command()
-@click.option('--topic', '-f')
+@click.option('--topic', '-f', help="Shows the saved pages under a topic")
 def SeePages(topic):
     """See all saved pages of a topic"""
     click.echo('There are '+ str(len(topics)) + ' topics')
-    print("\n".join(list(topics)))
+    click.echo("\n".join(list(topics)))
     if(topic == None):
-        print('Input a topic to see all the pages within it')
-        topic = input()
+        topic = click.prompt("Input a topic to see all the pages within it")
     
     if(not topic in topics):
-        print('No such topic exists')
+        click.echo('No such topic exists')
         return
 
-    for r, d, files in os.walk(topic):
-        for f in files:
-            file = open(topic+'/'+f, 'r')
-            print("Name-> " + file.readline()[:-1], "Link-> " + file.readline(), sep='\t')
+    for pages in dic_top_pag[topic]:
+        click.echo("Name-> " + pages + ", Link-> " + dic[pages][0])
+
+@main.command()
+@click.option('--topic', '-rt', help="Removes all saved pages under the topic")
+@click.option('--page', '-r', help="Removes specified saved page with specified name")
+@click.password_option()
+def remove(topic, page, password):
+    """If you run without any options this will delete all saved pages, run with --help to see usage"""
+    if((topic == None) and (page == None)):
+        os.chdir('../')
+        shutil.rmtree('blogs')
+    if(topic != None):
+        if(topic in topics):
+            shutil.rmtree(topic)
+        else:
+            click.echo("failed to remove the topic as it does not exist")
+    if(page != None):
+        if(page in dic):
+            os.chdir(dic[page])
+            os.remove(page)
 
 if __name__ == "__main__":
     main()
